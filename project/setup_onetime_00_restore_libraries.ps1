63e5b5145423d02af01ebabaac63e1fe21ff9014
cwinsor <cwinsor@gmail.com>
2019-10-01 08:32:41 +0000

    add NB4 - starting to look into the dataset (finally!)

--- project/setup_onetime_00_restore_libraries.ps1
+++ project/setup_onetime_00_restore_libraries.ps1
@@ -1,26 +1,9 @@
 
-# This powershell script sets up up a virtualenv for python libraries in this project
-# Run this once - when you first populate the project 
-# reference:  https://packaging.python.org/guides/installing-using-pip-and-virtualenv/
-
-#
-# confirm at least we have python with pip
-python --version
-python -m pip --version
-
-# add virtualenv if user doesn't have it
+# create an empty virtualenv and activate
 python -m pip install virtualenv
-python -m virtualenv --version
-
-# create an empty virtualenv
 python -m virtualenv pymote_env --no-site-packages
-
-# activate the (empty) virtualenv
 .\pymote_env\Scripts\activate
 
-# list what the blank environment has
-pip list
-
-# get the libraries specified in the requirements.txt file
+# get the packages
 pip install -r requirements.txt
 
